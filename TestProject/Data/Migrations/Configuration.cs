2f8721d2db9860b51a2624d827810a4df6980bd7
d.andrienko <danil.andrienko05@gmail.com>
2020-07-17 17:22:08 +0300

    Enabled Db auto migration

---
+++ TestProject/Data/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace Data.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<EmployeeManagementContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(EmployeeManagementContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
